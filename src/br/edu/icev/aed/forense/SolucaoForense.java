c58042d74ea7373aea1d1430d8b84b556d751e08
PedroJacquesDEV <pedro.jacques.academico@gmail.com>
2025-11-22 17:53:54 -0300

    Criação da classe SolucaoForense.java implementando a interface

--- src/br/edu/icev/aed/forense/SolucaoForense.java
+++ src/br/edu/icev/aed/forense/SolucaoForense.java
@@ -1,4 +1,37 @@
 package br.edu.icev.aed.forense;
 
-public class SolucaoForense {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class SolucaoForense implements AnaliseForenseAvancada {
+
+    public SolucaoForense() {
+    }
+
+    @Override
+    public Set<String> encontrarSessoesInvalidas(String arquivo) throws IOException {
+        return new HashSet<>(); // TODO: Implementar
+    }
+
+    @Override
+    public List<String> reconstruirLinhaTempo(String arquivo, String sessionId) throws IOException {
+        return new ArrayList<>(); // TODO: Implementar
+    }
+
+    @Override
+    public List<Alerta> priorizarAlertas(String arquivo, int n) throws IOException {
+        return new ArrayList<>(); // TODO: Implementar
+    }
+
+    @Override
+    public Map<Long, Long> encontrarPicosTransferencia(String arquivo) throws IOException {
+        return new HashMap<>(); // TODO: Implementar
+    }
+
+    @Override
+    public Optional<List<String>> rastrearContaminacao(String arquivo, String org, String dest) throws IOException {
+        return Optional.empty(); // TODO: Implementar
+    }
 }
